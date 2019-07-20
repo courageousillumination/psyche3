8fc7f4622983f6dd6fa8fd1f50f4ea8f60582b5d
Tristan Rasmussen <tristanr93@gmail.com>
2019-07-20 15:06:43 -0700

    Basic journal long renderer.

--- psyche/components/renderers/index.ts
+++ psyche/components/renderers/index.ts
@@ -3,17 +3,25 @@ import DefaultLongRenderer from "psyche/components/renderers/default/long-render
 import DefaultShortRenderer from "psyche/components/renderers/default/short-renderer";
 import { RendererGroup } from "psyche/components/renderers/types";
 import { Note } from "psyche/types/models";
+import JournalLongRenderer from "./journal/long-renderer";
 
 const GROUPS = {
   default: {
     edit: DefaultEditRenderer,
     long: DefaultLongRenderer,
     short: DefaultShortRenderer
+  },
+  journal: {
+    edit: DefaultEditRenderer,
+    long: JournalLongRenderer,
+    short: DefaultShortRenderer
   }
 };
 
 const getRendererGroup = (note: Note): RendererGroup => {
   switch (note.noteType) {
+    case "journal":
+      return GROUPS.journal;
     default:
       return GROUPS.default;
   }
